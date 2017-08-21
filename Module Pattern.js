5d860073675d9a4c6dff4fbc5d135e1e5f0eae5e
Abhishek Pandit <31212411+panditabhishek@users.noreply.github.com>
2017-08-21 19:54:19 +0530

    Create Module Pattern.js

---
+++ Module Pattern.js
@@ -0,0 +1,9 @@
+//Inital IIFE Implementation
+ (function(){
+ //....code goes here
+ })();
+ 
+//Making a global variable to access the code.
+ var ModuleDesign = (function(){
+ //....code goes here
+ })();
